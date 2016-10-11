5b1f6b4c0aedd7d8b1dc93c72ea9b46bdf76fa57
MorozovaLiuda <liuda315@gmail.com>
2016-10-11 17:18:13 +0300

    fix

--- apps/web/assets/javascripts/bootstrap.js
+++ apps/web/assets/javascripts/bootstrap.js
@@ -2781,9 +2781,7 @@ var Tooltip = (function ($) {
    * Check for Tether dependency
    * Tether - http://github.hubspot.com/tether/
    */
-  if (window.Tether === undefined) {
-    throw new Error('Bootstrap tooltips require Tether (http://github.hubspot.com/tether/)');
-  }
+
 
   /**
    * ------------------------------------------------------------------------
